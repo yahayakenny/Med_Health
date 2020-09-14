d316331b57eec9b62430c6b4df94292b06a98910
yahayakenny <y.hkehinde@yahoo.com>
2020-09-14 08:03:15 +0100

    added reset password page

--- doctor_app/urls.py
+++ doctor_app/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('<int:doctor_id>', views.doctor_detail, name='doctor_detail'),
     path('map/', views.map, name = 'map' ),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name ='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "patients/reset_password.html"), name ='reset_password'),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "patients/password_reset_sent.html"), name ='password_reset_done'),
+
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "patients/password_reset_form.html"), name ='password_reset_form'),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "patients/password_reset_done.html"), name ='password_reset_complete'),
 
 ]
