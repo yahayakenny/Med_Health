d91f1de210870a6134e8399de54c9b70c9a3aecd
yahayakenny <y.hkehinde@yahoo.com>
2020-10-27 21:59:49 +0100

    update postgresql database for heroku

--- med_health/settings.py
+++ med_health/settings.py
@@ -89,9 +89,12 @@ DATABASES = {
 
 }
 
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 AUTH_PASSWORD_VALIDATORS = [
