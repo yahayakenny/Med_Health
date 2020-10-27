7353cdcf146a5b7468ae36ba3296cb8bf4d894bc
yahayakenny <y.hkehinde@yahoo.com>
2020-10-27 22:42:49 +0100

    added postgresql database heroku

--- med_health/settings.py
+++ med_health/settings.py
@@ -90,12 +90,9 @@ DATABASES = {
 
 }
 
-# # import dj_database_url
-# # db_from_env = dj_database_url.config()
-# # DATABASES['default'].update(db_from_env)
-
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 AUTH_PASSWORD_VALIDATORS = [
